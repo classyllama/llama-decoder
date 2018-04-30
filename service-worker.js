6716fde1bb483df314af0c5be31f458b2a4c9121
Kevin Kirchner <kirchner.kevin@gmail.com>
2018-04-30 11:37:10 -0500

    add greg goodale's post id; update last screen

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 importScripts(
-  "/llama-decoder/precache-manifest.5d41efc98b727d4d55165fdff8791897.js"
+  "/llama-decoder/precache-manifest.85a97dbbaecd45ef91721bb11c286fc9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "llama-decoder"});
